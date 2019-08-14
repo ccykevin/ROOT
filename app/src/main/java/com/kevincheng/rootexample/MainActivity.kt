c1320189f9a112183a292667ec661233917db356
Kevin Cheng <kevin.cheng.development@gmail.com>
2019-08-14 13:35:00 +0800

    update the example app

--- app/src/main/java/com/kevincheng/rootexample/MainActivity.kt
+++ app/src/main/java/com/kevincheng/rootexample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.kevincheng.rootexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.orhanobut.logger.Logger
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Logger.d("DEBUG LOG")
     }
 }
