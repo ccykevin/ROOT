1d51ed7a58fb3fdd06cffc3c028ca8b53a293e97
Kevin Cheng <kevin.cheng.development@gmail.com>
2020-01-13 16:07:13 +0800

    remove the dependency of app extensions library

--- device-extensions/src/main/java/com/kevincheng/deviceextensions/Device.kt
+++ device-extensions/src/main/java/com/kevincheng/deviceextensions/Device.kt
@@ -1,26 +1,37 @@
 package com.kevincheng.deviceextensions
 
 import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.preference.PreferenceManager
 import android.provider.Settings
 import androidx.core.content.edit
-import com.kevincheng.appextensions.App
-import com.kevincheng.appextensions.Preferences
 import com.stericson.RootTools.RootTools
 import java.util.concurrent.TimeoutException
 
-class Device {
+class Device(private val applicationContext: Context) {
     companion object {
+        private lateinit var shared: Device
+
+        internal fun install(application: Application) {
+            when (::shared.isInitialized) {
+                false -> shared = Device(application.applicationContext)
+            }
+        }
+
         val androidId: String
             @SuppressLint("HardwareIds")
-            get() = Settings.Secure.getString(App.context.contentResolver, Settings.Secure.ANDROID_ID)
+            get() = Settings.Secure.getString(shared.applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
 
         val UUID: String
             get() {
-                var uuid = Preferences.getString(R.string.pref_deviceextensions_uuid_key, null)
+                val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(shared.applicationContext)
+                val key = shared.applicationContext.getString(R.string.pref_deviceextensions_uuid_key)
+                var uuid = sharedPreferences.getString(key, null)
                 if (uuid == null) {
                     uuid = java.util.UUID.randomUUID().toString()
-                    Preferences.get().edit(commit = true) {
-                        putString(App.context.getString(R.string.pref_deviceextensions_uuid_key), uuid)
+                    sharedPreferences.edit(commit = true) {
+                        putString(key, uuid)
                     }
                 }
                 return uuid
---
+++ device-extensions/src/main/java/com/kevincheng/deviceextensions/internal/DeviceExtensionsInstaller.kt
@@ -0,0 +1,33 @@
+package com.kevincheng.deviceextensions.internal
+
+import android.app.Application
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import com.kevincheng.deviceextensions.Device
+
+class DeviceExtensionsInstaller : ContentProvider() {
+
+    override fun onCreate(): Boolean {
+        val application = context!!.applicationContext as Application
+        Device.install(application)
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? = null
+
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int = 0
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int = 0
+}
