291ec8927304cf5556ed65e88d9eb44ac1d0f475
Kevin Cheng <kevin.cheng.development@gmail.com>
2020-08-14 20:17:23 +0800

    fix the bug that clip view does not work below API 21

--- widget/src/main/java/com/kevincheng/widget/rounded_corners/IRoundedView.kt
+++ widget/src/main/java/com/kevincheng/widget/rounded_corners/IRoundedView.kt
@@ -1,9 +1,5 @@
 package com.kevincheng.widget.rounded_corners
 
-import android.graphics.Canvas
-
 internal interface IRoundedView {
-    fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int)
-    fun dispatchDraw(canvas: Canvas)
     fun setCornerRadius(cornerRadius: Float)
 }
