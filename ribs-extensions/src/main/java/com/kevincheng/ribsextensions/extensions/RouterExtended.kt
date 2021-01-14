b479c9129399b4204cf47e3d1fded0d285ed4d25
Kevin Cheng <kevin.cheng.development@gmail.com>
2021-01-14 16:26:49 +0800

    enhance the RouterExtended and ViewRouterExtended classes to automatically handle the view provider's lifecycle event and automatically disposes all disposables

--- ribs-extensions/src/main/java/com/kevincheng/ribsextensions/extensions/RouterExtended.kt
+++ ribs-extensions/src/main/java/com/kevincheng/ribsextensions/extensions/RouterExtended.kt
@@ -4,6 +4,7 @@ import com.uber.rib.core.Interactor
 import com.uber.rib.core.InteractorBaseComponent
 import com.uber.rib.core.Router
 import com.uber.rib.core.screenstack.lifecycle.ScreenStackEvent
+import io.reactivex.disposables.CompositeDisposable
 
 @Suppress("FINITE_BOUNDS_VIOLATION_IN_JAVA")
 open class RouterExtended<I : Interactor<*, *>, C : InteractorBaseComponent<*>>(
@@ -11,20 +12,39 @@ open class RouterExtended<I : Interactor<*, *>, C : InteractorBaseComponent<*>>(
     component: C
 ) : Router<I, C>(interactor, component) {
 
-    private val attached = ArrayList<Router<*, *>>()
+    protected val disposables = CompositeDisposable()
+    protected val attachedViewProviderRouters = ArrayList<Router<*, *>>()
 
-    open fun handleScreenEvents(router: Router<*, *>, event: ScreenStackEvent) {
+    override fun willDetach() {
+        super.willDetach()
+        disposables.clear()
+        attachedViewProviderRouters.forEach { detachChild(it) }
+        attachedViewProviderRouters.clear()
+    }
+
+    protected open fun viewProviderWillAttach(vararg viewProviders: ViewProviderExtended) {
+        viewProviders.forEach { viewProvider ->
+            disposables.add(viewProvider
+                .lifecycle()
+                .subscribe { event ->
+                    handleScreenEvents(requireNotNull(viewProvider.router), event)
+                }
+            )
+        }
+    }
+
+    protected open fun handleScreenEvents(router: Router<*, *>, event: ScreenStackEvent) {
         when (event) {
             ScreenStackEvent.APPEARED -> {
-                if (attached.contains(router)) return
+                if (attachedViewProviderRouters.contains(router)) return
                 router.also {
                     attachChild(it)
-                    attached.add(it)
+                    attachedViewProviderRouters.add(it)
                 }
             }
             ScreenStackEvent.REMOVED -> router.also {
                 detachChild(it)
-                attached.remove(it)
+                attachedViewProviderRouters.remove(it)
             }
             else -> {
             }
--- ribs-extensions/src/main/java/com/kevincheng/ribsextensions/extensions/ViewRouterExtended.kt
+++ ribs-extensions/src/main/java/com/kevincheng/ribsextensions/extensions/ViewRouterExtended.kt
@@ -6,6 +6,7 @@ import com.uber.rib.core.InteractorBaseComponent
 import com.uber.rib.core.Router
 import com.uber.rib.core.ViewRouter
 import com.uber.rib.core.screenstack.lifecycle.ScreenStackEvent
+import io.reactivex.disposables.CompositeDisposable
 
 @Suppress("FINITE_BOUNDS_VIOLATION_IN_JAVA")
 open class ViewRouterExtended<V : View, I : Interactor<*, *>, C : InteractorBaseComponent<*>>(
@@ -14,20 +15,39 @@ open class ViewRouterExtended<V : View, I : Interactor<*, *>, C : InteractorBase
     component: C
 ) : ViewRouter<V, I, C>(view, interactor, component) {
 
-    private val attached = ArrayList<Router<*, *>>()
+    protected val disposables = CompositeDisposable()
+    protected val attachedViewProviderRouters = ArrayList<Router<*, *>>()
 
-    open fun handleScreenEvents(router: Router<*, *>, event: ScreenStackEvent) {
+    override fun willDetach() {
+        super.willDetach()
+        disposables.clear()
+        attachedViewProviderRouters.forEach { detachChild(it) }
+        attachedViewProviderRouters.clear()
+    }
+
+    protected open fun viewProviderWillAttach(vararg viewProviders: ViewProviderExtended) {
+        viewProviders.forEach { viewProvider ->
+            disposables.add(viewProvider
+                .lifecycle()
+                .subscribe { event ->
+                    handleScreenEvents(requireNotNull(viewProvider.router), event)
+                }
+            )
+        }
+    }
+
+    protected open fun handleScreenEvents(router: Router<*, *>, event: ScreenStackEvent) {
         when (event) {
             ScreenStackEvent.APPEARED -> {
-                if (attached.contains(router)) return
+                if (attachedViewProviderRouters.contains(router)) return
                 router.also {
                     attachChild(it)
-                    attached.add(it)
+                    attachedViewProviderRouters.add(it)
                 }
             }
             ScreenStackEvent.REMOVED -> router.also {
                 detachChild(it)
-                attached.remove(it)
+                attachedViewProviderRouters.remove(it)
             }
             else -> {
             }
