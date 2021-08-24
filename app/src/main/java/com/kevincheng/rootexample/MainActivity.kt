7471a040d4ffb00961bcf1ed7917ed96b703c766
Kevin Cheng <kevin.cheng.development@gmail.com>
2021-08-24 12:18:53 +0800

    update example app

--- app/src/main/java/com/kevincheng/rootexample/MainActivity.kt
+++ app/src/main/java/com/kevincheng/rootexample/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kevincheng.rootexample
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.kevincheng.appextensions.App
 import com.kevincheng.deviceextensions.Device
 import com.kevincheng.extensions.isGrantedRequiredPermissions
@@ -20,5 +22,26 @@ class MainActivity : AppCompatActivity() {
         Logger.d(Device.isRooted)
         Logger.d("Device.isPortrait@${Device.isPortrait} Device.isLandscape@${Device.isLandscape} Device.screenWidth@${Device.screenWidth} Device.screenHeight@${Device.screenHeight} Device.densityDpi@${Device.densityDpi} Device.density@${Device.density} Device.scaledDensity@${Device.scaledDensity} Device.smallestWidth@${Device.smallestWidth}")
         Logger.d("App.screenWidth@${App.screenWidth} App.screenHeight@${App.screenHeight} App.densityDpi@${App.densityDpi} App.density@${App.density} App.scaledDensity@${App.scaledDensity} App.smallestWidth@${App.smallestWidth}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!isGrantedRequiredPermissions) ActivityCompat.requestPermissions(
+            this,
+            requiredPermissions,
+            1
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when {
+            permissions.contains(Manifest.permission.REQUEST_INSTALL_PACKAGES) && !App.canRequestPackageInstalls -> App.grantRequestPackageInstallsPermission()
+            permissions.contains(Manifest.permission.SYSTEM_ALERT_WINDOW) && !App.canDrawOverlays -> App.grantDrawOverlaysPermission()
+        }
     }
 }
