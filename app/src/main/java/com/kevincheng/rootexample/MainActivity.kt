dd2386c292b9fde19d2ff556f97ed2d4c2744e49
Kevin Cheng <kevin.cheng.development@gmail.com>
2022-06-13 13:10:48 +0800

    update example app

--- app/src/main/java/com/kevincheng/rootexample/MainActivity.kt
+++ app/src/main/java/com/kevincheng/rootexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kevincheng.rootexample
 
 import android.Manifest
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -12,8 +13,13 @@ import com.kevincheng.widget.IRadioButton
 import com.kevincheng.widget.IRadioGroup
 import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(App.loadConfiguration(newBase))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +32,27 @@ class MainActivity : AppCompatActivity() {
         Logger.d("Device.isPortrait@${Device.isPortrait} Device.isLandscape@${Device.isLandscape} Device.screenWidth@${Device.screenWidth} Device.screenHeight@${Device.screenHeight} Device.densityDpi@${Device.densityDpi} Device.density@${Device.density} Device.scaledDensity@${Device.scaledDensity} Device.smallestWidth@${Device.smallestWidth}")
         Logger.d("App.screenWidth@${App.screenWidth} App.screenHeight@${App.screenHeight} App.densityDpi@${App.densityDpi} App.density@${App.density} App.scaledDensity@${App.scaledDensity} App.smallestWidth@${App.smallestWidth}")
 
-        rg.listener = object : IRadioGroup.Listener {
+        rg_language.listener = object : IRadioGroup.Listener {
             override fun onCheckedChanged(
                 radioGroup: IRadioGroup,
                 previousCheckedButton: IRadioButton?,
                 checkedButton: IRadioButton?
             ) {
-                Logger.d("previousCheckedButton@${previousCheckedButton?.value} checkedButton@${checkedButton?.value}")
+                val locale = (requireNotNull(checkedButton).value as String).split("_").let {
+                    when (it.size > 1) {
+                        true -> Locale(it[0], it[1])
+                        false -> Locale(it[0])
+                    }
+                }
+                val context = App.setLocale(locale)
+                textView_hello.text = context.getString(R.string.hello)
             }
+        }
+
+        when (Locale.getDefault().toString()) {
+            getString(R.string.lang_en) -> rg_language.checkNoEvent(R.id.rb_en)
+            getString(R.string.lang_hk) -> rg_language.checkNoEvent(R.id.rb_hk)
+            getString(R.string.lang_es) -> rg_language.checkNoEvent(R.id.rb_es)
         }
     }
 
